491261a73088aeebcb7fc004d09fd07cd6dddad9
salmawaleed055 <salmawaleed055@aucegypt.edu>
2025-03-26 01:45:34 +0200

    Added logs lines in the term class

--- src/term/Term.cpp
+++ src/term/Term.cpp
@@ -22,6 +22,7 @@ Term::Term(int DecimalValue, int numOfVariables) {
     onesCount = getOnesCount();
     combined = false;
     coveredTerms = {DecimalValue}; // The term covers itself initially
+
 }
 
 Term::Term(string BinaryValue, set<int> CoveredTerms) {
@@ -40,37 +41,45 @@ string Term::convertDecToBin(int numOfVariables) {
     string binaryStr = bitset<20>(decimalValue).to_string();
     // Extract only relevant bits according to the processed function
     return binaryStr.substr(20 - numOfVariables);
+    *log<<"decimal value:"<< decimalValue<<"binary value:"<<binaryValue<<endl;
 }
 
 int Term::getOnesCount() {
     return count(binaryValue.begin(), binaryValue.end(), '1');
+    *log<<"Ones count"<< count(binaryValue.begin(), binaryValue.end(), '1');;
 }
 
 // Getters
 int Term::getDecimalValue() {
     return decimalValue;
+    *log<<" get decimal value"<<decimalValue<<endl;
 }
 
 string Term::getBinaryValue() {
     return binaryValue;
+    *log<<"get binary value"<<binaryValue<<endl;
 }
 
 bool Term::isCombined() {
     return combined;
+    *log<<"is combined "<<combined<<endl;
 }
 
 // TODO: test suite
 set<int> Term::getCoveredTerms() {
     return coveredTerms;
+    *log<<"get covered terms:"<<coveredTerms;
 }
 
 // Setters
 void Term::_setLogger(Logger* logger) {
     log = logger;
+    *log<<"set logger";
 }
 
 void Term::setCombined(bool val) {
     combined = val;
+    *log<<"set combined";
 }
 
 // Methods
