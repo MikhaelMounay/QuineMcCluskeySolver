4b2659dda6c79b5e532e350014b1aaa279ed19b3
mikhaelmounay <mikhaelmounay@gmail.com>
2025-03-26 22:05:43 +0200

    (fix) main: fix some input validation and error handling for Command Line Arguments handling

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,13 @@ int main(int argc, char* argv[]) {
     string inputFilepath;
     string outputFilepath;
     string logFilepath;
+    string verilogFilepath;
 
     // Handling Command Line Arguments
     if (argc < 2) {
         cerr << "Usage: " << argv[0] <<
-            "[-v] [-d <dump_log_file>] [-o <verilog_file>] <input_file>" <<
+            "[-v] [-d <dump_log_file>] [-o <output_file>] [--verilog <verilog_file>] <input_file>"
+            <<
             endl;
         return 1;
     }
@@ -23,27 +25,51 @@ int main(int argc, char* argv[]) {
         if (string(argv[i]) == "-v") {
             verbose = true;
         } else if (string(argv[i]) == "-d") {
-            if (string(argv[i + 1]).empty()) {
-                cerr << "Error: Log file path must be provided after -o" <<
+            if (string(argv[i + 1]).empty() || string(argv[i + 1]).starts_with("-")) {
+                cerr << "Error: Log file path must be provided after -d" <<
                     endl;
-                cerr << "Usage: " << argv[0] <<
-                    "[-v] [-d <dump_log_file>] [-o <verilog_file>] <input_file>"
-                    << endl;
+                cout << "Usage: " << argv[0] <<
+                    "[-v] [-d <dump_log_file>] [-o <output_file>] [--verilog <verilog_file>] <input_file>"
+                    <<
+                    endl;
                 return 1;
             }
 
-            outputFilepath = string(argv[i]);
+            logFilepath = string(argv[i + 1]);
+            i++;
         } else if (string(argv[i]) == "-o") {
-            if (string(argv[i + 1]).empty()) {
+            if (string(argv[i + 1]).empty() || string(argv[i + 1]).starts_with("-")) {
                 cerr << "Error: Output file path must be provided after -o" <<
                     endl;
-                cerr << "Usage: " << argv[0] <<
-                    "[-v] [-d <dump_log_file>] [-o <verilog_file>] <input_file>"
-                    << endl;
+                cout << "Usage: " << argv[0] <<
+                    "[-v] [-d <dump_log_file>] [-o <output_file>] [--verilog <verilog_file>] <input_file>"
+                    <<
+                    endl;
                 return 1;
             }
 
-            outputFilepath = string(argv[i]);
+            outputFilepath = string(argv[i + 1]);
+            i++;
+        } else if (string(argv[i]) == "--verilog") {
+            if (string(argv[i + 1]).empty() || string(argv[i + 1]).starts_with("-")) {
+                cerr << "Error: Output Verilog file path must be provided after --verilog" <<
+                    endl;
+                cout << "Usage: " << argv[0] <<
+                    "[-v] [-d <dump_log_file>] [-o <output_file>] [--verilog <verilog_file>] <input_file>"
+                    <<
+                    endl;
+                return 1;
+            }
+
+            verilogFilepath = string(argv[i + 1]);
+            i++;
+        } else if (string(argv[i]).starts_with("-")) {
+            cerr << "Error: Unknown flag: " << string(argv[i]) << endl;
+            cout << "Usage: " << argv[0] <<
+                    "[-v] [-d <dump_log_file>] [-o <output_file>] [--verilog <verilog_file>] <input_file>"
+                    <<
+                    endl;
+            return 1;
         } else {
             inputFilepath = string(argv[i]);
             if (inputFilepath.empty()) {
@@ -58,8 +84,15 @@ int main(int argc, char* argv[]) {
     }
 
     log << "Hello, World!\n";
+    // Read input file
 
-    // Write to log file
+    // Output to console
+
+    // Output to _minimized.txt file
+
+    // Output Verilog to .v file
+
+    // Write log to dump file
     if (!outputFilepath.empty()) {
         ofstream logFile(outputFilepath);
         if (!logFile.is_open()) {
